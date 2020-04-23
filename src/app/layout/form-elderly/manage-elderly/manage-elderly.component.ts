1c1f5da4b8a0dcfc9765016d1d276ffae78f1f75
amnuay <am-nuay2010@hotmail.com>
2020-04-23 12:45:48 +0700

    ดาต้า table เหลือคลิกน่ะ

--- src/app/layout/form-elderly/manage-elderly/manage-elderly.component.ts
+++ src/app/layout/form-elderly/manage-elderly/manage-elderly.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProfileModel } from '../../model/profile-model/profile-model.module';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -6,6 +6,7 @@ import { ApiServiceModule } from '../../api-service/api-service.module';
 import { ProfileModelModule } from '../../model/profile-model/profile-model.module';
 import { map, catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { API_PROFILE_URL } from 'src/app/shared/config/constants';
 
 @Component({
   selector: 'app-manage-elderly',
@@ -14,17 +15,82 @@ import { Router } from '@angular/router';
 })
 export class ManageElderlyComponent implements OnInit {
   profileModels: Array<ProfileModel> = [];
-
+  @ViewChild('dataTable') table;
+  dataTable: any;
+  tableData: Profile[] = [];
+  dtOptions: any;
   constructor(
     private http: HttpClient,
     private fb: FormBuilder,
     private apiService: ApiServiceModule,
     private profileModelModule: ProfileModelModule,
-    public router: Router) {
-      this.ngOnInit();
+    public router: Router) { }
+
+
+  ngOnInit() {
+    // this.onLoadDate();
+    this.getProfile();
+
+  }
+
+  getProfile() {
+    this.http.get(API_PROFILE_URL + '/get-profile-list-by-role/' + '2').subscribe(
+      (data: Profile[]) => {
+        this.tableData = data;
+        this.dtOptions = {
+          data: this.tableData,
+          pagingType: 'full_numbers',
+          pageLength: 15,
+          processing: true,
+          // order: [0, 'asc'],
+          // dom: 'Bfrtip',
+          // buttons: [
+          //   'copy', 'csv', 'excel', 'print'
+          // ],
+          columns: [
+            { title: 'ชื่อบัญชี', data: 'userName' },
+            { title: 'ชื่อ - สกุล', data: 'fullName' },
+            { title: 'ชุมชน', data: 'community' },
+            { title: 'สิทธิ์', data: 'role' },
+            { title: '', defaultContent: '<button onclick="ddd()" type="button" class="btn btn-warning">แก้ไข</button>' },
+            {
+              title: '',
+              data: null,
+              orderable: true,
+              render: function (data1, type, item, meta) {
+                return '<button  (click)="testClick()" type="button" class="btn btn-danger">ลบ</button>';
+              }
+            },
+            {
+              data: 'MachineNumber', 'width': '50px', 'render': function (data22) {
+                return '<button (click)="testClick()" type="button" class="btn btn-primary">testClick</button>';
+              }
+            }
+          ],
+        };
+        $(document).on('click', 'tr', function () {
+          $('.username').text('');
+          $('.username').text($(this).parents('tr').find('.fname').text());
+          alert('clicked!');
+        });
+      }, err => { }, () => {
+        this.dataTable = $(this.table.nativeElement);
+        this.dataTable.DataTable(this.dtOptions);
+
+      });
+  }
+
+  ddd() {
+    console.log('ssss');
+    alert('ddd');
+  }
+
+  testClick() {
+    console.log('xxxx');
+    alert('testClick');
   }
   onLoadDate() {
-    this.apiService.getProfileListByRole("2").subscribe(
+    this.apiService.getProfileListByRole('2').subscribe(
       () => {
         this.profileModels = this.profileModelModule._profileModels;
       }, (err) => {
@@ -39,8 +105,8 @@ export class ManageElderlyComponent implements OnInit {
   deleteProfile(userId?: String) {
     this.apiService.deleteProfile(userId);
     // this.ngOnInit();
-    this.router.navigateByUrl('/RefrshComponent', {skipLocationChange: true}).then(()=>
-    this.router.navigate(['/form-elderly/manage-elderly']));
+    this.router.navigateByUrl('/RefrshComponent', { skipLocationChange: true }).then(() =>
+      this.router.navigate(['/form-elderly/manage-elderly']));
 
   }
   //  getProfileListByRole(role : String): Observable<any> {
@@ -69,8 +135,32 @@ export class ManageElderlyComponent implements OnInit {
   //   }
   // }
 
-  ngOnInit() {
-    this.onLoadDate();
-  }
 
+
+}
+
+
+export interface Profile {
+  userId: string;
+  userName: string;
+  passWord?: any;
+  fullName: string;
+  role: string;
+  roleName?: any;
+  cardId?: any;
+  titleName?: any;
+  fertName?: any;
+  lastName?: any;
+  age?: any;
+  phoneNo?: any;
+  sex?: any;
+  district?: any;
+  houseNo?: any;
+  postal?: any;
+  province?: any;
+  subdistrict?: any;
+  community: string;
+  img?: any;
+  lat?: any;
+  lng?: any;
 }
