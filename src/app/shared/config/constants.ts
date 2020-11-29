0e1bda4dc509c4c4a011228dbc9ad101aad49707
top <TopZa007>
2020-11-29 16:55:28 +0700

    set  api localhost 8090

--- src/app/shared/config/constants.ts
+++ src/app/shared/config/constants.ts
@@ -9,7 +9,7 @@ export const HTTP_TIME_OUT = 10000; // millisecond
 
 
 export const API_EPAYMENT = 'http://10.11.2.25:8770/rd-epayment-service/epay/';
-//export const API_EPAYMENT = 'http://localhost:8770/epay/';
+// export const API_EPAYMENT = 'http://localhost:8770/epay/';
 
 
 export const ERROR_001 = 'load data fail';
@@ -17,11 +17,11 @@ export const SUCCESS = 'success';
 export const AUTH_KEY = '123';
 
 // profile
-  // export const API_PROFILE_URL = `http://localhost:8090/profile/`;
-  // export const API_ASSESS_URL = `http://localhost:8090/assess/`;
+  export const API_PROFILE_URL = `http://localhost:8090/profile/`;
+  export const API_ASSESS_URL = `http://localhost:8090/assess/`;
 
-  export const API_PROFILE_URL = `http://103.22.183.133:8085/server/profile/`;
-  export const API_ASSESS_URL = `http://103.22.183.133:8085/server/assess/`;
+  // export const API_PROFILE_URL = `http://103.22.183.133:8085/server/profile/`;
+  // export const API_ASSESS_URL = `http://103.22.183.133:8085/server/assess/`;
   export const USER_ID_KEY = 'userId';
   export const USER_MODEL_KEY = 'userModel';
   export const USER_MODEL_KEYT = 'userModel';
