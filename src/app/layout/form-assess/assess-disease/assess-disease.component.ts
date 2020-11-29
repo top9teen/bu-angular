5cd37dc2019c2cb4aac4dad9bc63f9c73914aa67
top <TopZa007>
2020-11-29 16:54:14 +0700

    update code

--- src/app/layout/form-assess/assess-disease/assess-disease.component.ts
+++ src/app/layout/form-assess/assess-disease/assess-disease.component.ts
@@ -1,11 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { ApiServiceModule } from '../../api-service/api-service.module';
-import { InspectionModel, InspectionModelModule, QuestionModel, ChoiceModel } from '../../model/inspection-model/inspection-model';
-import { map, catchError } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Jsonp } from '@angular/http';
+import { ApiServiceModule } from '../../api-service/api-service.module';
+import { InspectionModel, InspectionModelModule, QuestionModel } from '../../model/inspection-model/inspection-model';
 
 @Component({
   selector: 'app-assess-disease',
@@ -26,14 +24,14 @@ export class AssessDiseaseComponent implements OnInit {
   inspectionId: String;
   inspectionModel?: InspectionModel;
   questions: Array<QuestionModel> = [];
-  result: Array<submitQ> = [];
+  result: Array<SubmitQ> = [];
   inspectionName: String;
-  checkQ: number = 0;
-  disabled: boolean = false;
-  assessmentId:number = 0;
-  Q2: boolean = false;
-  Q8: boolean = false;
-  Q9: boolean = false;
+  checkQ = 0;
+  disabled = false;
+  assessmentId = 0;
+  Q2 = false;
+  Q8 = false;
+  Q9 = false;
 
   inspectionForm: FormGroup = this.fb.group({
     inspectionId: this.fb.control('', Validators.required),
@@ -60,7 +58,7 @@ export class AssessDiseaseComponent implements OnInit {
           this.inspectionModel = this.inspectionModelModule._inspectionModel;
           this.questions = this.inspectionModel.questions;
           this.inspectionName = this.inspectionModel.inspectionName;
-          if(Q==8){
+          if (Q === 8) {
             this.addFromSubmit(this.assessmentId);
           }
         }, (err) => {
@@ -72,10 +70,10 @@ export class AssessDiseaseComponent implements OnInit {
 
   getInspection() {
     this.inspectionId = this.inspectionForm.value.inspectionId;
-    if(this.inspectionId === '2'){
+    if (this.inspectionId === '2') {
       this.checkQ = 2;
       this.Q2 =  true;
-    }else{
+    } else {
       this.checkQ = 0;
       this.Q2 = false;
       this.Q8 = false;
@@ -122,7 +120,7 @@ export class AssessDiseaseComponent implements OnInit {
   }
 
  onSubmitForm2Q() {
-    let answer = {} as submitQ;
+    let answer = {} as SubmitQ;
     const sizeChoice = parseFloat((document.getElementById('sizeChoice') as HTMLInputElement).value);
       for (let i = 0; i < sizeChoice; i++) {
         const red = document.getElementsByName('criterion_' + i);
@@ -136,19 +134,19 @@ export class AssessDiseaseComponent implements OnInit {
             const y: number = +x;
             answer.question_id = y;
             this.result.push(answer);
-            answer = {} as submitQ;
+            answer = {} as SubmitQ;
           }
         }
       }
-      for(let j = 0; j < this.questions.length; j++){
-        this.result[j].question_id =+ this.inspectionId;
+      for (let j = 0; j < this.questions.length; j++) {
+        this.result[j].question_id = + this.inspectionId;
         this.result[j].assessmentId = this.assessmentId;
       }
-      console.log('result :: ' + JSON.stringify(this.result))
+      console.log('result :: ' + JSON.stringify(this.result));
     this.apiService.getsaveAssess2Q(this.result).subscribe(
-      (res: resultSubmit) => {
+      (res: ResultSubmit) => {
         console.log(JSON.stringify(res));
-        if(res.assessmentDetail.includes('เป็นผู้มีความเสี่ยง')){
+        if (res.status) {
             this.Q2 =  false;
             this.Q8 =  false;
             this.Q9 = true;
@@ -156,7 +154,7 @@ export class AssessDiseaseComponent implements OnInit {
             this.assessmentId = res.assessmentId;
             this.getQuestion(this.checkQ);
             this.addFromSubmit(this.assessmentId);
-        }else{
+         } else {
           this.router.navigateByUrl('/RefrshComponent', {skipLocationChange: true}).then(() =>
           this.router.navigate(['/form-assess/assess-ment']));
         }
@@ -168,7 +166,7 @@ export class AssessDiseaseComponent implements OnInit {
   }
 
   onSubmitForm9Q() {
-    let answer = {} as submitQ;
+    let answer = {} as SubmitQ;
     const sizeChoice = parseFloat((document.getElementById('sizeChoice') as HTMLInputElement).value);
       for (let i = 0; i < sizeChoice; i++) {
         const red = document.getElementsByName('criterion_' + i);
@@ -179,25 +177,25 @@ export class AssessDiseaseComponent implements OnInit {
             answer.answer = t;
             answer.userId = localStorage.getItem('userId');
             this.result.push(answer);
-            answer = {} as submitQ;
+            answer = {} as SubmitQ;
           }
         }
       }
-      for(let j = 0; j < this.questions.length; j++){
-        this.result[j].question_id =+ this.inspectionId;
+      for (let j = 0; j < this.questions.length; j++) {
+        this.result[j].question_id = + this.inspectionId;
         this.result[j].assessmentId = this.assessmentId;
       }
-      console.log('result :: ' + JSON.stringify(this.result))
+      console.log('result :: ' + JSON.stringify(this.result));
     this.apiService.getsaveAssess9Q(this.result).subscribe(
-      (res: resultSubmit) => {
-        if(res.criterionTotal >= 7){
+      (res: ResultSubmit) => {
+        if (res.status) {
             this.Q2 =  false;
             this.Q8 =  true;
             this.Q9 = false;
             this.checkQ = 8;
             this.assessmentId = res.assessmentId;
             this.getQuestion(this.checkQ);
-        }else{
+        } else {
           this.router.navigateByUrl('/RefrshComponent', {skipLocationChange: true}).then(() =>
           this.router.navigate(['/form-assess/assess-ment']));
         }
@@ -209,18 +207,18 @@ export class AssessDiseaseComponent implements OnInit {
   }
 
   onSubmitForm8Q() {
-    for(let j = 0; j < this.questions.length; j++){
-      this.result[j].question_id =+ this.inspectionId;
+    for (let j = 0; j < this.questions.length; j++) {
+      this.result[j].question_id = + this.inspectionId;
     }
-    console.log('result :: ' + JSON.stringify(this.result))
+    console.log('result :: ' + JSON.stringify(this.result));
 
     this.apiService.getsaveAssess2Q(this.result).subscribe(
-      (res: resultSubmit) => {
-        if(res.criterionTotal >= 17){
-          alert('ส่งต่อโรงพยาบาลมีจิตแพทย์ด่วน');
+      (res: ResultSubmit) => {
+        if (res.status) {
+          alert(res.detail);
           this.router.navigateByUrl('/RefrshComponent', {skipLocationChange: true}).then(() =>
           this.router.navigate(['/form-assess/assess-ment']));
-        }else{
+        } else {
           this.router.navigateByUrl('/RefrshComponent', {skipLocationChange: true}).then(() =>
           this.router.navigate(['/form-assess/assess-ment']));
         }
@@ -241,53 +239,53 @@ export class AssessDiseaseComponent implements OnInit {
   //     var d = document.getElementsByName("criterion_0");
   //  }
 
-  addFromSubmit(assessmentId: number){
+  addFromSubmit(assessmentId: number) {
     this.result = [];
-    if(this.questions.length > 0){
-      let answer = {} as submitQ;
-      for(let j = 0; j < this.questions.length; j++){
-          answer.question_id =+ this.questions[j].questionId;
+    if (this.questions.length > 0) {
+      let answer = {} as SubmitQ;
+      for (let j = 0; j < this.questions.length; j++) {
+          answer.question_id = + this.questions[j].questionId;
           answer.userId = localStorage.getItem('userId');
           answer.assessmentId = assessmentId;
           this.result.push(answer);
-          answer = {} as submitQ;
+          answer = {} as SubmitQ;
       }
     }
   }
 
-  onChange(index: number, data: string, questId: number){
-    console.log("index input :: " + index);
-    console.log("questId  :: " + questId);
-    console.log("data input :: " + data);
-    let texterror = /^[0-9]+$/;
-    console.log("data if :: " + texterror.test(data));
+  onChange(index: number, data: string, questId: number) {
+    console.log('index input :: ' + index);
+    console.log('questId  :: ' + questId);
+    console.log('data input :: ' + data);
+    const texterror = /^[0-9]+$/;
+    console.log('data if :: ' + texterror.test(data));
     if (!texterror.test(data)) {
-      console.log("data if ===============" )
-      if(index == 3){
+      console.log('data if ===============' );
+      if (index === 3) {
         this.disabled = false;
       }
-      if(this.result.length > 0){
-        for(let j = 0; j < this.result.length; j++){
-          if(this.result[j].question_id == questId){
+      if (this.result.length > 0) {
+        for (let j = 0; j < this.result.length; j++) {
+          if (this.result[j].question_id === questId) {
               this.result[j].answer = 0;
             break;
           }
         }
       }
-    }else{
-        if(index == 3){
+    } else {
+        if (index === 3) {
           this.disabled = true;
         }
-      if(this.result.length > 0){
-        for(let j = 0; j < this.result.length; j++){
-          if(this.result[j].question_id == questId){
-              this.result[j].answer =+ data;
+      if (this.result.length > 0) {
+        for (let j = 0; j < this.result.length; j++) {
+          if (this.result[j].question_id === questId) {
+              this.result[j].answer = + data;
             break;
           }
         }
       }
     }
-    console.log('result :: ' + JSON.stringify(this.result))
+    console.log('result :: ' + JSON.stringify(this.result));
   }
 
   onBack() {
@@ -304,25 +302,15 @@ export class AssessDiseaseComponent implements OnInit {
   }
 }
 
-interface submitQ {
+interface SubmitQ {
   assessmentId: number;
   answer: number;
   question_id: number;
   userId: string;
 }
 
-interface a {
-  answer: number;
-  question_id: number;
-  userId: string;
-}
-
-interface resultSubmit{
-  assessmentDetail: string;
+interface ResultSubmit {
   assessmentId: number;
-  createDate: Date;
-  inspetionDetail: string;
-  userId: string;
-  inspectionId: number;
-  criterionTotal: number;
+  status: boolean;
+  detail: string;
 }
